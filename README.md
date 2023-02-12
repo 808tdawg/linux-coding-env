194d6b9948ecb14305fe07178d999a44d4f4535d
Tausif Islam <tausif.islam90@gmail.com>
2023-02-11 22:34:58 -1000

    first commit, main script and README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # linux-coding-env
-automated coding env set up
+This script sets up a 64-bit ARM Ubuntu Linux environment with the latest version of Python, Visual Studio Code, Poetry, Docker, and the tree command. The script uses `sudo` for privileged operations, and the password is stored in the `PASSWORD` variable and passed to `sudo` as required. 
+
+## Steps performed in the script:
+1. Installs Python 3.10 using apt-get and creates a symbolic link to python.
+2. Downloads Visual Studio Code for Linux ARM64 in .deb format using curl and installs it using dpkg.
+3. Modifies the list of favorite apps in GNOME Shell by adding the terminal and Visual Studio Code and removing Thunderbird Mail, Rhythmbox, and LibreOffice Writer.
+4. Installs Poetry using the command-line installer and adds it to the PATH.
+5. Installs Docker using apt-get and starts and enables the Docker service.
+6. Restarts the system.
+
+### Note:
+- The script uses `sudo` for privileged operations and passes the password stored in the `PASSWORD` variable to `sudo` using `echo $PASSWORD | sudo -S` before each command that requires elevated privileges.
+
+### Simple installation steps in terminal:
+'curl -O ' 
+'bash letscode.sh'
---
+++ letscode.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Define the password variable
+PASSWORD="requiredpassword"
+
+# Install Python 3.10 with sudo and pass the password to sudo
+echo $PASSWORD | sudo -S apt-get update
+echo $PASSWORD | sudo -S apt-get install -y python3.10
+sudo ln -s $(which python3) /usr/bin/python
+
+# Download Visual Studio Code for Linux ARM64 .deb package
+curl -L -o vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64"
+
+# Install the package using dpkg with sudo and pass the password to sudo
+echo $PASSWORD | sudo -S dpkg -i vscode.deb
+
+# Automatically answer yes to any prompts during the installation
+yes | sudo apt-get install -f
+
+# Remove the .deb package file with sudo and pass the password to sudo
+echo $PASSWORD | sudo -S rm vscode.deb
+
+# Get the current list of favorites
+favorites=$(gsettings get org.gnome.shell favorite-apps)
+
+# Add the terminal and Visual Studio Code to the list of favorites
+favorites="${favorites::-1},'gnome-terminal.desktop','code.desktop']"
+
+# Remove Thunderbird Mail, Rhythmbox, LibreOffice Writer, and Help from the list of favorites
+favorites=$(echo $favorites | sed "s/'thunderbird.desktop',//g;s/'rhythmbox.desktop',//g;s/'libreoffice-writer.desktop',//g;s/'gnome-help.desktop',//g")
+
+# Set the new list of favorites
+gsettings set org.gnome.shell favorite-apps "$favorites"
+
+# Install Poetry
+curl -sSL https://install.python-poetry.org | python3 -
+
+# Add Poetry to PATH
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+# Install Docker with sudo and pass the password to sudo
+echo $PASSWORD | sudo -S apt-get update
+echo $PASSWORD | sudo -S apt-get install -y docker.io
+
+# Install the tree command with sudo and pass the password to sudo
+echo $PASSWORD | sudo -S apt-get install -y tree
+
+# Start the Docker service and enable it to start at boot time with sudo and pass the password to sudo
+echo $PASSWORD | sudo -S systemctl start docker
+echo $PASSWORD | sudo -S systemctl enable docker
+
+# Restart the system with sudo and pass the password to sudo
+echo $PASSWORD | sudo -S reboot
